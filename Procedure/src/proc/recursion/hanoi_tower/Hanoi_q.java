adafd080e5e5b2a10e0ff38d4541f2acec8c151a
GemiBillyBob <i.m.pycco@gmail.com>
2013-01-20 01:01:42 +0100

    Settings

---
+++ Procedure/src/proc/recursion/hanoi_tower/Hanoi_q.java
@@ -0,0 +1,34 @@
+package proc.recursion.hanoi_tower;
+
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 20.01.13
+ * Time: 1:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hanoi_q {
+
+    public static void main(String[] args) {
+        Stack<Integer> from = new Stack<>();
+        Stack<Integer> help = new Stack<>();
+        Stack<Integer> to = new Stack<>();
+        from.push(3);
+        from.push(2);
+        from.push(1);
+        exchange(from, help, to, from.size());
+    }
+
+    public static void exchange(
+            Stack<Integer> from, Stack<Integer> help,
+            Stack<Integer> to, int count) {
+        if (count > 0) {
+           // exchange(?, ?, ?, ?);
+            int biggest = from.pop();
+            to.push(biggest);
+            //exchange(?, ?, ?, ?);
+        }
+    }
+}
