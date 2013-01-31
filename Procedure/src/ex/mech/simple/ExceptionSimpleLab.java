14e37d01dc92b98e7b29fc2fdc96b77e17d716a0
GemiBillyBob <i.m.pycco@gmail.com>
2013-01-31 22:10:12 +0100

    Exceptions. Mechanics. Simple.

--- Procedure/src/ex/mech/simple/ExceptionSimpleLab.java
+++ Procedure/src/ex/mech/simple/ExceptionSimpleLab.java
@@ -1,11 +1,35 @@
 package ex.mech.simple;
 
-/**
- * Created with IntelliJ IDEA.
- * User: admin
- * Date: 28.01.13
- * Time: 23:45
- * To change this template use File | Settings | File Templates.
- */
 public class ExceptionSimpleLab {
+    public static void main(String[] args) {
+
+        System.out.print(0);
+        try {
+            System.out.print(1);
+            if (true) {throw new Error();}
+            System.out.print(2);
+        } catch (RuntimeException e) {
+            System.out.print(3);
+            if (true) {throw new Error();}
+            System.out.print(4);
+        } finally {
+            System.out.print(5);
+            if (true) {throw new RuntimeException();}
+            System.out.print(6);
+        }
+        System.out.print(7);
+    }
 }
+      /*
+
+0125
+012567
+01345
+0134567
+0135
+01356
+015
+0156
+
+
+            */
