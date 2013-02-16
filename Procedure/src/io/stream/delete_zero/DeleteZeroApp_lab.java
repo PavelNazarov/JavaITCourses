91913b77e6e8c145492f15860f57a25cd43d06c1
GemiBillyBob <i.m.pycco@gmail.com>
2013-02-16 13:04:08 +0100

    io.stream.delete_zero

--- Procedure/src/io/stream/delete_zero/DeleteZeroApp_lab.java
+++ Procedure/src/io/stream/delete_zero/DeleteZeroApp_lab.java
@@ -9,21 +9,40 @@ import java.io.*;
  */
 public class DeleteZeroApp_lab {
     public static void main(String[] args) throws IOException {
-        InputStream in = new FileInputStream("d:/java.txt");
-        OutputStream out = new FileOutputStream("d:/java_out.txt", false);
+        int buffSize = 10;
+        InputStream in = null;
+        OutputStream out = null;
+        IOException ioException;
         long startTime = System.currentTimeMillis();
-        while (true) {
-            int value = in.read();
-            if (value > 0) {
-                out.write(value);
-            } else if (value == -1) {
-                break;
+
+        try {
+            in = new FileInputStream("d:/java.txt");
+            out = new FileOutputStream("d:/java_out.txt", false);
+            while (true) {
+                byte[] buff = new byte[buffSize];
+                int value = in.read(buff);
+
+                if (value == -1) break;
+                int lastZeroIndex = 0;
+                for (int i = 0; i < buff.length; i++) {
+                    if (buff[i] == 0 || i == buff.length - 1) {
+                        if (i - lastZeroIndex > 0) out.write(buff, lastZeroIndex, i - lastZeroIndex);
+                        lastZeroIndex = i + 1;
+                    }
+                }
             }
+        } catch (IOException e) {
+            ioException = e;
+            new java.io.File("d:/java_out.txt").delete();
+            throw ioException;
+        } finally {
+            long deltaTime = System.currentTimeMillis() - startTime;
+            if (in != null) in.close();
+            if (out != null) {
+                out.flush();
+                out.close();
+            }
+            System.out.println(deltaTime);
         }
-        long deltaTime = System.currentTimeMillis() - startTime;
-        in.close();
-        out.flush();
-        out.close();
-        System.out.println(deltaTime);
     }
 }
