3b03577289b09613134ae4df1efcb4f979778fa9
GemiBillyBob <i.m.pycco@gmail.com>
2013-08-08 01:00:41 +0300

    threads and tested bubble sort

--- Procedure/src/tests/Test.java
+++ Procedure/src/tests/Test.java
@@ -7,9 +7,36 @@ package tests;
  */
 public class Test {
     public static void main(String[] args) {
-            Parent child = new Child();
-            Receiver advancedReceiver = new AdvancedReceiver();
-            advancedReceiver.receive(child);
+        Parent child = new Child();
+        Receiver advancedReceiver = new AdvancedReceiver();
+        advancedReceiver.receive(child);
+
+        B b = new B();
+        A a = b;
+        a.f1();
+        a.f2();
+        ((B) a).f3();
+        ///  a.f3();
+    }
+}
+
+class A {
+    public void f1() {
+
+    }
+
+    public void f2() {
+
+    }
+}
+
+class B extends A {
+    public void f3() {
+
+    }
+
+    public void f2() {
+
     }
 }
 
@@ -32,7 +59,7 @@ class Receiver {
     }
 }
 
-class AdvancedReceiver extends Receiver{
+class AdvancedReceiver extends Receiver {
     public void receive(Child c) {
         System.out.println("AdvancedReceiver");
         c.outMe();
